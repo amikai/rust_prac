3c760cd945c7217f1d1b771d1914f7d36f6f9afc
amikai <as23041248@gmail.com>
2023-01-23 00:20:33 +0800

    init project: hello world axum http service

---
+++ axum_web/src/main.rs
@@ -0,0 +1,27 @@
+use axum::{routing, Router};
+use tower_http::trace::TraceLayer;
+
+use anyhow::Result;
+use tower::ServiceBuilder;
+
+#[derive(Clone)]
+struct State {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let r = Router::new().route("/hello", routing::get(hello)).layer(
+        ServiceBuilder::new()
+            // Add high level tracing/logging to all requests
+            .layer(TraceLayer::new_for_http()),
+    );
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(r.into_make_service())
+        .await?;
+    Ok(())
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
